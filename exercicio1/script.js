dc6456b29350fcdcb99f10abc0dfdbeba456ba85
mayaracgc <mayaracgcgmail.com>
2022-09-07 15:06:20 -0300

    exercicio feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = prompt("Digite um número.");
 
-if(){
-    console.log("sua mensagem aqui");
+if (num % 2 === 0) {
+  console.log("Seu número é par");
+} else {
+  console.log("Seu número é ímpar");
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,4 +1,4 @@
-let idade;
+/*let idade;
 let isMaiorIdade;
 let isCursandoOutraFaculdade;
 
@@ -6,4 +6,24 @@ if(){
 
 }else{
 	
+}
+*/
+const idade = Number (prompt("Qual a sua idade?"));
+let colegial = confirm("Você terminou o ensino médio?");
+let faculdade = confirm("Você cursa faculdade?");
+
+if (idade >= 18) {
+  console.log("Você é maior de idade.");
+} else {
+    console.log("Você não é maior de idade.")
+}
+if (colegial == true) {
+  console.log("Você já terminou o ensino médio!");
+} else {
+    console.log("Você não terminou o ensino médio.")
+}
+if (faculdade == true) {
+  console.log("Você faz faculdade!");
+} else {
+  console.log("Você não é maior de idade");
 }
